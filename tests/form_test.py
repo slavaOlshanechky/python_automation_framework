02184851523e381da622cff565a391aafb888eee
sl <olshanechky@gmail.com>
2024-03-23 18:44:57 +0200

    Added base_page, conftest, form_test

---
+++ tests/form_test.py
@@ -0,0 +1,13 @@
+from pages.form_page import FormPage
+
+
+class TestFormPage:
+    def test_form(self, driver):
+        form_page = FormPage(driver, 'https://demoqa.com/automation-practice-form')
+        form_page.open()
+        person = form_page.fill_fields_and_submit()
+        result = form_page.form_result()
+        print(person)
+        print(result)
+        assert f'{person.first_name}{person.last_name}' == result[0], 'the form has not been filled'
+        assert person.email == result[1], 'the form has not been filled'
